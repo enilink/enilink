590aa7e0b88c373fabfa3f64a0b0139180bb632e
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2012-06-21 17:34:38 +0200

    Allow for right alignment of menu entries.
    Change-Id: I1a6c7af512e6d3bf54fefaac9278472b55d36b07

--- bundles/web/net.enilink.lift/src/main/scala/net/enilink/lift/snippet/Bootstrap.scala
+++ bundles/web/net.enilink.lift/src/main/scala/net/enilink/lift/snippet/Bootstrap.scala
@@ -78,7 +78,10 @@ class Bootstrap extends DispatchSnippet {
           </li>
       }
     }
-    <ul class="nav"> { for (item <- menuEntries) yield renderItem(item) } </ul>
+    val items = menuEntries
+    def pullRight(item : MenuItem) = item.cssClass.exists(_ == "pull-right")
+    <ul class="nav"> { for (item <- items.filterNot(pullRight(_))) yield renderItem(item) } </ul>
+    <ul class="nav pull-right"> { for (item <- items.filter(pullRight(_))) yield renderItem(item) } </ul>
   }
 
   def submenu: NodeSeq = {
