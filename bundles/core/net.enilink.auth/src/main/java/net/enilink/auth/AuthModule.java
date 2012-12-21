5e1915ce70c5d1eb870a4ed0bd126ec1bc958f2d
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2012-12-21 11:33:53 +0100

    Ensure correct classpath for OSGi and non-OSGi applications.
    Change-Id: I5e8bfc2fe401f0e0ea33338a7c034ecda53ad38a

--- bundles/core/net.enilink.auth/src/main/java/net/enilink/auth/AuthModule.java
+++ bundles/core/net.enilink.auth/src/main/java/net/enilink/auth/AuthModule.java
@@ -2,15 +2,13 @@ package net.enilink.auth;
 
 import net.enilink.vocab.acl.Authorization;
 import net.enilink.komma.core.KommaModule;
-import net.enilink.komma.util.KommaUtil;
 import net.enilink.komma.util.RoleClassLoader;
 
 public class AuthModule extends KommaModule {
 	{
 		// include FOAF concepts
 		RoleClassLoader loader = new RoleClassLoader(this);
-		loader.load(KommaUtil
-				.getBundleMetaInfLocations("net.enilink.vocab.foaf"));
+		loader.load();
 
 		// include ACL concepts
 		addConcept(Authorization.class);
