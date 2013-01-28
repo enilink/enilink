a971796e5629cd28d42a6e98e0364ce5419cf0e8
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2013-01-28 20:50:48 +0100

    Add standalone mode to the eniLINK login module.
    Change-Id: I4503dab10d327df76c691b31af4dc0c5d7e0d57a

--- bundles/core/net.enilink.auth/src/main/java/net/enilink/auth/AccountHelper.java
+++ bundles/core/net.enilink.auth/src/main/java/net/enilink/auth/AccountHelper.java
@@ -8,8 +8,11 @@ import java.util.List;
 import javax.security.auth.Subject;
 
 import net.enilink.vocab.auth.AUTH;
+import net.enilink.vocab.foaf.FOAF;
 import net.enilink.commons.iterator.IMap;
 import net.enilink.commons.iterator.WrappedIterator;
+import net.enilink.komma.concepts.IResource;
+import net.enilink.komma.core.IEntity;
 import net.enilink.komma.core.IEntityManager;
 import net.enilink.komma.core.IReference;
 import net.enilink.komma.core.IStatement;
@@ -21,6 +24,44 @@ import net.enilink.komma.core.URIImpl;
  * Helper class for managing user accounts.
  */
 public class AccountHelper {
+	/**
+	 * Creates a new user resources.
+	 * 
+	 * @param em
+	 *            The entity manager to use
+	 * @param username
+	 *            The name of the user (nickname)
+	 * @return The user resource
+	 * @throws IllegalArgumentException
+	 *             If the user with the given id already exists in the database.
+	 */
+	public static synchronized IEntity createUser(IEntityManager em,
+			String username) throws IllegalArgumentException {
+		URI userId = getUserURI(username);
+		// TODO Is it possible to use database locking here?
+		if (em.createQuery("ask { ?user ?p ?o }").setParameter("user", userId)
+				.getBooleanResult()) {
+			throw new IllegalArgumentException(
+					"A user with this name already exists.");
+		}
+		// create user and add nickname
+		IEntity user = em.createNamed(userId, FOAF.TYPE_AGENT);
+		user.as(IResource.class).addProperty(FOAF.PROPERTY_NICK, username);
+		return user;
+	}
+
+	/**
+	 * Returns the user's URI for the given user name.
+	 * 
+	 * @param username
+	 *            The user's name.
+	 * @return The user's URI.
+	 */
+	public static URI getUserURI(String username) {
+		return URIImpl.createURI("http://enilink.net/users").appendLocalPart(
+				URIImpl.encodeOpaquePart(username, false));
+	}
+
 	/**
 	 * Returns external IDs from the given subject.
 	 * 
@@ -35,7 +76,7 @@ public class AccountHelper {
 	public static List<URI> getExternalIds(Subject subject) {
 		List<URI> externalIds = new ArrayList<URI>();
 		for (Principal principal : subject.getPrincipals()) {
-			if (!(principal instanceof Group)) {
+			if (!(principal instanceof Group || principal instanceof UserPrincipal)) {
 				URI externalId = URIImpl
 						.createURI("urn:jaas:principal:"
 								+ principal.getClass().getName()
