88723c30d4219f15b453b0335fcb5702fe2f85e5
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2014-04-24 17:56:59 +0200

    Added method to change acl:owner and minor fixes.
    Change-Id: I0bb1aed30842f2b87b2140d568d4d7b85ddf7895

--- bundles/core/net.enilink.core/src/main/java/net/enilink/core/security/ISecureEntity.java
+++ bundles/core/net.enilink.core/src/main/java/net/enilink/core/security/ISecureEntity.java
@@ -1,16 +1,15 @@
 package net.enilink.core.security;
 
-import net.enilink.vocab.foaf.Agent;
-
 import net.enilink.composition.annotations.Iri;
-
 import net.enilink.komma.core.IEntity;
 import net.enilink.komma.core.IReference;
 
 @Iri("http://www.w3.org/2000/01/rdf-schema#Resource")
 public interface ISecureEntity extends IEntity {
 	@Iri("http://www.w3.org/ns/auth/acl#owner")
-	Agent getAclOwner();
+	IReference getAclOwner();
+	
+	void setAclOwner(IReference agent);
 
 	boolean hasAclMode(IReference user, IReference mode);
 }
