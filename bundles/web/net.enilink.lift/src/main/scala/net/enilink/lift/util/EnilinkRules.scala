bc0f3928dde7aad33406e3795b14325d3d02d532
Marko Friedemann <marko.friedemann@iwu.fraunhofer.de>
2015-03-31 15:44:06 +0200

    Load security-related rules from config objects.
    default.ttl replacement for the (now deleted) static list of login methods:
    @base <plugin://net.enilink.lift/EnilinkRules/> .
    <> <loginModule> [ <jaasConfigName> "eniLINK" ; rdfs:label "eniLINK" ] ,
            [ <jaasConfigName> "CMIS" ; rdfs:label "IWU Share" ] ,
            [ <jaasConfigName> "OpenID" ; rdfs:label "OpenID" ] .

--- bundles/web/net.enilink.lift/src/main/scala/net/enilink/lift/util/EnilinkRules.scala
+++ bundles/web/net.enilink.lift/src/main/scala/net/enilink/lift/util/EnilinkRules.scala
@@ -3,12 +3,35 @@ package net.enilink.lift.util
 import net.liftweb.http.Factory
 import java.util.regex.Pattern
 import org.eclipse.core.runtime.Platform
+import net.enilink.komma.core.URIs
+import net.liftweb.common.Box
+import java.net.URL
+import scala.collection.JavaConversions._
+import net.enilink.komma.core.IReference
+import net.enilink.vocab.rdfs.RDFS
+import net.liftweb.common.Empty
+import net.liftweb.common.Full
 
 object EnilinkRules extends Factory {
-  val JAAS_CONFIG_URL = Platform.getBundle("net.enilink.core").getResource("/resources/jaas.conf");
+
+  val configURI = URIs.createURI("plugin://net.enilink.lift/EnilinkRules/")
+
+  val JAAS_CONFIG_URL = Globals.config.vend flatMap { cfg =>
+    Box.legacyNullTest(cfg.filter(configURI, configURI.appendLocalPart("jaasConfigUrl"), null).objectString) map { new URL(_) }
+  } openOr Platform.getBundle("net.enilink.core").getResource("/resources/jaas.conf")
+
   val REQUIRE_LOGIN = false || "true".equalsIgnoreCase(System.getProperty("enilink.loginrequired"))
 
-  val LOGIN_METHODS = List(("eniLINK", "eniLINK"), ("IWU Share", "CMIS"), ("OpenID", "OpenID"))
+  val LOGIN_METHODS: List[(String, String)] = Globals.config.vend.toList flatMap { cfg =>
+    cfg.filter(configURI, configURI.appendLocalPart("loginModule"), null).objects collect {
+      case module: IReference =>
+        val name = Box.legacyNullTest(cfg.filter(module, configURI.appendLocalPart("jaasConfigName"), null).objectString)
+        val label = Box.legacyNullTest(cfg.filter(module, RDFS.PROPERTY_LABEL, null).objectString)
+        name map { n =>
+          (label openOr n, n)
+        }
+    } flatten
+  }
 
   /**
    * The regular expression pattern for matching email addresses.
