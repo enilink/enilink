731542f32b602cb8262b153285781c705b3197f4
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2016-01-28 17:33:35 +0100

    Windows services support (WIP)
    Change-Id: Idbb3cc761d09cc34b6b02116b6f53d239990530e

---
+++ features/net.enilink.daemon.feature/service/service-start.bat
@@ -0,0 +1,32 @@
+@echo off
+set APP_DIR=%~dp0
+
+call %APP_DIR%\setenv.bat 
+
+echo Starting service %SERVICE_NAME%
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+SC start %SERVICE_NAME%
