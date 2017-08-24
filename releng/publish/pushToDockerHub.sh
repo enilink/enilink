7363cb134a394efacaf3dc7591f678a6914a3aa8
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2017-08-24 14:41:02 +0200

    Add script for pushing to Docker hub.
    Change-Id: I9826a036258e7c6cbbf2cd54188d8e11663c43e7

---
+++ releng/publish/pushToDockerHub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+SCRIPT_PATH=`dirname $0`
+
+TAG=$(git describe --tags --exact-match 2>/dev/null)
+TAG=${TAG#v}
+
+VERSION_FILE="$SCRIPT_PATH/../../features/net.enilink.platform.updatesite/target-maven/repository/version.txt"
+DOCKER_BUILD_CONTEXT="$SCRIPT_PATH/../../features/net.enilink.platform.product"
+
+if [ ! -z "$TAG" ]; then
+# use current tag as version
+  VERSION=$TAG
+elif [ -e "$VERSION_FILE" ]; then
+# read version from file
+  VERSION=$(<"$VERSION_FILE")
+fi
+
+REPO=enilink/enilink
+
+docker build "$DOCKER_BUILD_CONTEXT" -t "$REPO:$VERSION"
+docker tag "$REPO:$VERSION" "$REPO:latest"
+
+# login to Docker registry if user and password are given as environment variables
+if [ ! -z "$DOCKER_USER" ] && [! -z "$DOCKER_PASS"]; then
+  docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+fi
+
+docker push "$REPO"
