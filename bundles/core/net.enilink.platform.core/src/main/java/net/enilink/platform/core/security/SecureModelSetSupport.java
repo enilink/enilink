6c3fd5d9ffd28e0a93c6519bf5eb1a059ce0d347
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2020-10-07 15:28:28 +0200

    Use model ACLs also for audit models and correctly invalidate cache on ACL changes.

--- bundles/core/net.enilink.platform.core/src/main/java/net/enilink/platform/core/security/SecureModelSetSupport.java
+++ bundles/core/net.enilink.platform.core/src/main/java/net/enilink/platform/core/security/SecureModelSetSupport.java
@@ -1,33 +1,33 @@
 package net.enilink.platform.core.security;
 
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
 
+import com.google.inject.*;
 import net.enilink.composition.annotations.ParameterTypes;
 import net.enilink.composition.cache.annotations.Cacheable;
 import net.enilink.composition.traits.Behaviour;
-import net.enilink.komma.core.IEntityManager;
-import net.enilink.komma.core.IReference;
-import net.enilink.komma.core.URI;
+import net.enilink.komma.common.notify.INotification;
+import net.enilink.komma.common.notify.INotificationListener;
+import net.enilink.komma.common.notify.NotificationFilter;
+import net.enilink.komma.core.*;
 import net.enilink.komma.dm.IDataManager;
 import net.enilink.komma.em.ThreadLocalDataManager;
 import net.enilink.komma.model.IModel;
 import net.enilink.komma.model.IModelSet;
+import net.enilink.komma.model.base.ModelSetSupport;
 import net.enilink.komma.model.concepts.ModelSet;
+import net.enilink.komma.model.event.IStatementNotification;
 import net.enilink.vocab.acl.ENILINKACL;
 import net.enilink.vocab.acl.WEBACL;
 
 import org.aopalliance.intercept.MethodInvocation;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import com.google.inject.Module;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import com.google.inject.util.Modules;
 
-public abstract class SecureModelSetSupport implements ISecureModelSet,
+public abstract class SecureModelSetSupport implements IModelSet.Internal, ISecureModelSet,
 		Behaviour<ISecureModelSet> {
+	protected EntityVar<INotificationListener<INotification>> metaDataListener;
+
 	static class SecureThreadLocalDataManager extends ThreadLocalDataManager {
 		@Inject
 		ISecureModelSet modelSet;
@@ -41,7 +41,7 @@ public abstract class SecureModelSetSupport implements ISecureModelSet,
 	/**
 	 * Ensures that an ACL aware data manager is used to access the models.
 	 */
-	@ParameterTypes({ Collection.class })
+	@ParameterTypes({Collection.class})
 	public void collectInjectionModules(MethodInvocation invocation)
 			throws Throwable {
 		invocation.proceed();
@@ -69,7 +69,33 @@ public abstract class SecureModelSetSupport implements ISecureModelSet,
 		modules.add(compoundModule);
 	}
 
-	@ParameterTypes({ URI.class, String.class })
+	public void init(Injector injector) {
+		metaDataListener.set(new INotificationListener<INotification>() {
+			NotificationFilter<INotification> filter = NotificationFilter
+					.instanceOf(IStatementNotification.class);
+			Set<URI> properties = new HashSet<>(Arrays.asList(WEBACL.PROPERTY_AGENT, WEBACL.PROPERTY_AGENTCLASS, WEBACL.PROPERTY_ACCESSTO,
+					WEBACL.PROPERTY_ACCESSTOCLASS, WEBACL.PROPERTY_MODE, WEBACL.PROPERTY_OWNER));
+
+			@Override
+			public void notifyChanged(Collection<? extends INotification> notifications) {
+				notifications.stream().forEach(notification -> {
+					IStatementNotification stmtNotification = (IStatementNotification) notification;
+					if (properties.contains(stmtNotification.getPredicate())) {
+						// refresh cached properties of this model set
+						getMetaDataManager().refresh(getBehaviourDelegate());
+					}
+				});
+			}
+
+			@Override
+			public NotificationFilter<INotification> getFilter() {
+				return filter;
+			}
+		});
+		getBehaviourDelegate().addMetaDataListener(metaDataListener.get());
+	}
+
+	@ParameterTypes({URI.class, String.class})
 	public IModel createModel(MethodInvocation invocation) throws Throwable {
 		IModel model = (IModel) invocation.proceed();
 		if (model != null) {
@@ -88,8 +114,8 @@ public abstract class SecureModelSetSupport implements ISecureModelSet,
 	public boolean isReadableBy(IReference model, IReference agent) {
 		if (model == null
 				|| model.equals(((IModelSet.Internal) getBehaviourDelegate())
-						.getDefaultGraph())
-				|| model.equals(((ModelSet)getBehaviourDelegate()).getMetaDataContext())
+				.getDefaultGraph())
+				|| model.equals(((ModelSet) getBehaviourDelegate()).getMetaDataContext())
 		) {
 			return true;
 		}
@@ -99,9 +125,19 @@ public abstract class SecureModelSetSupport implements ISecureModelSet,
 			return true;
 		}
 		Set<IReference> modes = secureEntity.getAclModes(agent);
-		return modes.contains(WEBACL.MODE_READ)
+		boolean isReadable = modes.contains(WEBACL.MODE_READ)
 				|| modes.contains(ENILINKACL.MODE_WRITERESTRICTED)
 				|| modes.contains(WEBACL.MODE_CONTROL);
+		if (!isReadable) {
+			// use the model's ACLs also for the audit models
+			isReadable = Optional.ofNullable(model.getURI())
+					.map(uri -> uri.toString())
+					.filter(uriStr -> uriStr.startsWith("enilink:audit:"))
+					.map(uriStr -> URIs.createURI(uriStr.substring("enilink:audit:".length())))
+					.map(uri -> getBehaviourDelegate().isReadableBy(uri, agent))
+					.orElse(false);
+		}
+		return isReadable;
 	}
 
 	@Override
@@ -109,10 +145,10 @@ public abstract class SecureModelSetSupport implements ISecureModelSet,
 	public IReference writeModeFor(IReference model, IReference agent) {
 		if (model == null
 				|| model.equals(((IModelSet.Internal) getBehaviourDelegate())
-						.getDefaultGraph())) {
+				.getDefaultGraph())) {
 			return null;
 		}
-		if (model.equals(((ModelSet)getBehaviourDelegate()).getMetaDataContext())) {
+		if (model.equals(((ModelSet) getBehaviourDelegate()).getMetaDataContext())) {
 			return WEBACL.MODE_WRITE;
 		}
 		ISecureEntity secureEntity = getMetaDataManager().findRestricted(model,
